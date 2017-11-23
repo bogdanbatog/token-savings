521c1a31c710e74eba2bdb583584c01cc1188a3d
Bogdan <bogdanbatog@gmail.com>
2017-11-23 12:05:38 +0200

    fix first test

--- test/TestSaveBling.js
+++ test/TestSaveBling.js
@@ -1,7 +1,8 @@
-// var BigNumber = require('bignumber.js');
+var BigNumber = require('bignumber.js');
 
 // Specifically request an abstraction for SaveBling
 var SaveBling = artifacts.require("SaveBling");
+var gasPrice = 100;
 
 contract('SaveBling', function(accounts) {
 
@@ -9,36 +10,29 @@ contract('SaveBling', function(accounts) {
     var account_one = accounts[0];
     var initial_balance = web3.eth.getBalance(account_one);
 
-    console.log(initial_balance.toString());
-    var sb;
-    var gasUsed = 0;
+    var saveBling;
+    var gasUsed = new BigNumber(0);
   
     return SaveBling.deployed().then(function(instance) {
-      sb = instance;
-      return sb.send(web3.toWei(0, "ether"));
+      saveBling = instance;
+      return saveBling.send(web3.toWei(1, "ether"));
     }).then(function(result) {
 
-      console.log(result);
-      gasUsed = web3.toWei(result.receipt.gasUsed, 'Gwei');
-      console.log(gasUsed);
-
+      gasUsed = gasUsed.add(web3.toWei(result.receipt.gasUsed * gasPrice, 'Gwei'));
       var mid_balance = web3.eth.getBalance(account_one);
-      console.log(initial_balance.minus(gasUsed).minus(mid_balance).toString());
       assert.equal(
         mid_balance.toString(),
         initial_balance.minus(web3.toWei(1, 'ether')).minus(gasUsed).toString(),
         "mid balance != initial balance - 1 ether - gasUsed"
       );
-      return sb.send(web3.toWei(0, "ether"));
+      return saveBling.send(web3.toWei(0, "ether"));
 
     }).then(function(result) {
 
-      gasUsed = gasUsed.plus(web3.toWei(result.receipt.gasUsed, 'Gwei'));
-      console.log(gasUsed);
-
+      gasUsed = gasUsed.add(web3.toWei(result.receipt.gasUsed * gasPrice, 'Gwei'));
       var final_balance = web3.eth.getBalance(account_one);
       assert.equal(
-        final_balance, initial_balance - gasUsed,
+        final_balance.toString(), initial_balance.minus(gasUsed).toString(),
         "final balance != initial balance - gasUsed"
       );
 
