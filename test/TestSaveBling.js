f01e87fb65a9648cb4b0d51820cbfb1dff8da713
Bogdan <bogdanbatog@gmail.com>
2017-11-23 23:41:58 +0200

    use events for second test too

--- test/TestSaveBling.js
+++ test/TestSaveBling.js
@@ -40,51 +40,35 @@ contract('SaveBling', function(accounts) {
 
 
   it("test two users", function() {
+    var saveBling;
     var acct_A = accounts[0];
     var acct_B = accounts[1];
-    var initial_balance_A = web3.eth.getBalance(acct_A);
-    var initial_balance_B = web3.eth.getBalance(acct_B);
-    var gasUsed_A = new BigNumber(0);
-    var gasUsed_B = new BigNumber(0);
-
-    var saveBling;
+    var ret_A;
+    var ret_B;
 
     return SaveBling.deployed().then(function(instance) {
       saveBling = instance;
       return saveBling.sendTransaction({value: web3.toWei(1, "ether"), from: acct_A});
     }).then(function(result) {
-      gasUsed_A = gasUsed_A.add(web3.toWei(result.receipt.gasUsed * gasPrice, 'Gwei'));
-
       return saveBling.sendTransaction({value: web3.toWei(1, "ether"), from: acct_B});
     }).then(function(result) {
-      gasUsed_B = gasUsed_B.add(web3.toWei(result.receipt.gasUsed * gasPrice, 'Gwei'));
-
-      var mid_balance_A = web3.eth.getBalance(acct_A);
-      var mid_balance_B = web3.eth.getBalance(acct_B);
-
       return saveBling.sendTransaction({value: web3.toWei(0, "ether"), from: acct_A});
     }).then(function(result) {
-      gasUsed_A = gasUsed_A.add(web3.toWei(result.receipt.gasUsed * gasPrice, 'Gwei'));
-
+      assert.equal(
+        result.logs[0].event, 'WithdrawalMade',
+        "WithdrawalMade event did not fired"
+      );
+      ret_A = result.logs[0].args.value;
       return saveBling.sendTransaction({value: web3.toWei(0, "ether"), from: acct_B});
     }).then(function(result) {
-      gasUsed_B = gasUsed_B.add(web3.toWei(result.receipt.gasUsed * gasPrice, 'Gwei'));
-
-      var final_balance_A = web3.eth.getBalance(acct_A);
-      var final_balance_B = web3.eth.getBalance(acct_B);
-
       assert.equal(
-        initial_balance_A.add(initial_balance_B).toString(),
-        final_balance_A.add(final_balance_B).add(gasUsed_A).add(gasUsed_B).toString(),
-        "final balance != initial balance - gasUsed"
+        result.logs[0].event, 'WithdrawalMade',
+        "WithdrawalMade event did not fired"
       );
+      ret_B = result.logs[0].args.value;
 
-      assert.equal(
-        final_balance_B.minus(initial_balance_B).add(gasUsed_B).toString(),
-        web3.toWei(0.025, "ether"),
-        "final balance != initial balance - gasUsed"
-      );
-
+      assert.equal(ret_A.add(ret_B).toString(), web3.toWei(2, "ether"));
+      assert.equal(ret_B.toString(), web3.toWei(1.025, "ether"));
     })
   });
 
